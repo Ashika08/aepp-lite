f3909b781b0f4cfec251ee59dd8d670396c30f00
Ashika Manjunath <amanjunath@adobe.com>
2022-12-19 13:30:07 +0100

    added access control and audit endpoints

--- constants.py
+++ constants.py
@@ -1,6 +1,15 @@
 # constants.py
 
 JWT_TOKEN_ENDPOINT = 'https://ims-na1.adobelogin.com/ims/exchange/jwt'
-DATASET_SERVICE_ENDPOINT = 'https://platform.adobe.io/data/foundation/dataset/datasets'
+
+ACCESS_CONTROL_ACCESS_CONTROL_POLICIES_PERMISSION_AND_RESOURCE_ENDPOINT = 'https://platform.adobe.io/data/foundation/access-control/acl/reference'
+ACCESS_CONTROL_ACCESS_CONTROL_POLICIES_EFFECTIVE_POLICIES_ENDPOINT = 'https://platform.adobe.io/data/foundation/access-control/acl/effective-policies'
+
+AUDIT_QUERY_AUDIT_EVENTS_ENDPOINT = 'https://platform.adobe.io/data/foundation/audit/events'
+AUDIT_QUERY_EXPORT_AUDIT_EVENTS_ENDPOINT = 'https://platform.adobe.io/data/foundation/audit/export'
+
 CATALOG_SERVICE_BATCHES_ENDPOINT = 'https://platform.adobe.io/data/foundation/catalog/batches'
+
+DATASET_SERVICE_ENDPOINT = 'https://platform.adobe.io/data/foundation/dataset/datasets'
+
 QUERY_SERVICE_QUERIES_ENDPOINT = 'https://platform.adobe.io/data/foundation/query/queries'
